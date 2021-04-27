6fac649591de4b74feaf4e234f876a2100d5dce2
pnautiy <pnautiy>
2021-04-27 02:37:15 +0000

    commit in singapore git

--- myfile.java
+++ myfile.java
@@ -3,5 +3,6 @@ public class MyClass{
 	public static void main(String []args){
 		System.out.println("Hello World");
 		System.out.println("My name is Swapnil Gawade");
+		System.out.println("Currently I am working in TCS");
 	}
 }
